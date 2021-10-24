3db605fd9653bd11546cc75f53caba8f98524674
sakshi <sakshi17098gupta@gmail.com>
2021-10-24 19:32:43 +0530

    my first commit react website

---
+++ src/Components/Home.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import styled from 'styled-components'
+import Section from './Section'
+
+function Home() {
+    return (
+        <Container>
+           <Section 
+                title="Model S"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-s.jpg"
+                leftBtnText="custom order"
+                rightBtnText="Existing inventory"
+           />
+           <Section 
+                title="Model Y"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-y.jpg"
+                leftBtnText="custom order"
+                rightBtnText="Existing inventory"
+           />
+           <Section 
+                title="Model 3"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-3.jpg"
+                leftBtnText="custom order"
+                rightBtnText="Existing inventory"
+           />
+           <Section 
+                title="Model X"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="model-x.jpg"
+                leftBtnText="custom order"
+                rightBtnText="Existing inventory"
+           />
+           <Section 
+                title="Lowest Cost Solar Panels in America"
+                description="Money-back Guarantee"
+                backgroundImg="solar-panel.jpg"
+                leftBtnText="order now"
+                rightBtnText="Learn more"
+           />
+           <Section 
+                title="Solar for New Roof"
+                description="Solar Roof Costs Less Than a New Roof"
+                backgroundImg="solar-roof.jpg"
+                leftBtnText="order now"
+                rightBtnText="Learn more"
+           />
+           <Section 
+                title="Accessories"
+                backgroundImg="accessories.jpg"
+                leftBtnText="Shop now"
+           />
+           
+        </Container>
+    )
+}
+
+export default Home
+
+const Container = styled.div`
+    height: 100vh;
+`
